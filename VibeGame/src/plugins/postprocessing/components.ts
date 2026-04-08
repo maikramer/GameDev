34c3cb46038e0c7855e589947987962a3fddd09a
Maikeu Locatelli <maikramer@gmail.com>
2026-04-08 15:38:17 -0300

    feat(vibegame): add vignette, dof, chromatic-aberration, noise components
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- VibeGame/src/plugins/postprocessing/components.ts
+++ VibeGame/src/plugins/postprocessing/components.ts
@@ -3,6 +3,7 @@ import { defineComponent, Types } from 'bitecs';
 export const Bloom = defineComponent({
   intensity: Types.f32,
   luminanceThreshold: Types.f32,
+  luminanceSmoothing: Types.f32,
   mipmapBlur: Types.ui8,
   radius: Types.f32,
   levels: Types.ui8,
@@ -26,4 +27,29 @@ export const Tonemapping = defineComponent({
 
 export const SMAA = defineComponent({
   preset: Types.ui8,
+});
+
+export const Vignette = defineComponent({
+  darkness: Types.f32,
+  offset: Types.f32,
+});
+
+export const DepthOfField = defineComponent({
+  focusDistance: Types.f32,
+  focalLength: Types.f32,
+  bokehScale: Types.f32,
+  resolutionScale: Types.f32,
+  autoFocus: Types.ui8,
+});
+
+export const ChromaticAberration = defineComponent({
+  offsetX: Types.f32,
+  offsetY: Types.f32,
+  radialModulation: Types.ui8,
+  modulationOffset: Types.f32,
+});
+
+export const Noise = defineComponent({
+  opacity: Types.f32,
+  blendFunction: Types.ui8,
 });
