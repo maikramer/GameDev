b148360345c0bbe9866afa1f631a100522bbd6eb
Maikeu Locatelli <maikramer@gmail.com>
2026-04-09 20:06:20 -0300

    fix(i18n): add XML recipe and key adapter
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- VibeGame/src/plugins/i18n/index.ts
+++ VibeGame/src/plugins/i18n/index.ts
@@ -1,4 +1,5 @@
 export { I18nText } from './components';
 export { I18nPlugin } from './plugin';
+export { i18nTextRecipe } from './recipes';
 export { I18nResolveSystem } from './systems';
 export { getLocale, loadDictionary, setLocale, t } from './utils';
